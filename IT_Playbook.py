478a39bef56df0f1b26c09523544be3857e3e96f
stubhohm <123859155+stubhohm@users.noreply.github.com>
2024-04-09 20:05:03 -0500

    Making Playbook for IT Issues

---
+++ IT_Playbook.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import Tk, ttk, messagebox, filedialog
+import sqlite3
+import re
+import os
+
+
+
+def main():
+    active_session = True
+    while active_session:
+        
+        print('running playbook')
+
+    print('closing playbook')
+
+if __name__ == '__main__':
+    main()
